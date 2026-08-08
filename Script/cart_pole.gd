8c34b158290cd05c4907d32a63ed1b584e84f3a6
nonyx777 <nahomretebo@gmail.com>
2026-08-08 19:18:44 +0300

    downgraded to godot 4.6.3 | pole is upright by default, and reset linear and angular velocities of both cart and pole at ready

--- Script/cart_pole.gd
+++ Script/cart_pole.gd
@@ -1,6 +1,14 @@
 extends Node3D
 
 @onready var cart: RigidBody3D = $Cart
+@onready var pole: RigidBody3D = $Pole
+
+func _ready() -> void:
+	pole.linear_velocity = Vector3.ZERO
+	pole.angular_velocity = Vector3.ZERO
+	cart.linear_velocity = Vector3.ZERO
+	cart.angular_velocity = Vector3.ZERO
+	
 
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("left"):
