0f1c1ca67f124e38e8d7fc17de5f1717cf6d2e28
nonyx777 <nahomretebo@gmail.com>
2026-08-09 21:25:25 +0300

    Trainable CartPole rl environment

---
+++ Scene/ai_controller_3d.gd
@@ -0,0 +1,43 @@
+extends AIController3D
+
+
+@onready var cart: RigidBody3D = $"../Cart-Pole/Cart"
+@onready var pole: RigidBody3D = $"../Cart-Pole/Pole"
+
+var slide: int = 0
+var pole_angle: float = 0
+var cart_pos_x: float = 0
+
+func get_pole_angle() -> float:
+	var pole_dir: Vector3 = (pole.position - cart.position).normalized()
+	return atan2(pole_dir.dot(Vector3.RIGHT), pole_dir.dot(Vector3.UP))
+
+func get_pole_angular_velocity() -> float:
+	var angular_velocity: float = pole.angular_velocity.dot(Vector3.FORWARD)
+	return angular_velocity
+
+func get_obs() -> Dictionary:
+	pole_angle = get_pole_angle()
+	cart_pos_x = cart.position.x
+	var pole_angular_velocity: float = get_pole_angular_velocity()
+	var obs: Array = [
+		cart_pos_x,
+		cart.linear_velocity.x,
+		pole_angle,
+		pole_angular_velocity
+	]
+	return {"obs":obs}
+
+func get_reward() -> float:	
+	return reward
+	
+func get_action_space() -> Dictionary:
+	return {
+		"slide" : {
+			"size": 2,
+			"action_type": "discrete"
+		},
+		}
+	
+func set_action(action) -> void:
+	slide= int(action["slide"])
--- Script/cart_pole.gd
+++ Script/cart_pole.gd
@@ -1,17 +1,58 @@
 extends Node3D
 
-@onready var cart: RigidBody3D = $Cart
-@onready var pole: RigidBody3D = $Pole
+@onready var cart: RigidBody3D = $"Cart-Pole/Cart"
+@onready var pole: RigidBody3D = $"Cart-Pole/Pole"
 
-func _ready() -> void:
+@onready var ai_controller: AIController3D = $AIController3D
+
+const MAX_POLE_ANGLE: float = deg_to_rad(30)
+const MAX_CART_DIST: float = 2.5
+const MAX_NUM_STEPS: int = 200
+const CART_FORCE: float = 5.0
+
+var pole_default_transform: Transform3D
+const cart_default_position: Vector3 = Vector3.ZERO
+
+func reset_values() -> void:
+	pole.transform = pole_default_transform
+	cart.position = cart_default_position
+	
 	pole.linear_velocity = Vector3.ZERO
 	pole.angular_velocity = Vector3.ZERO
 	cart.linear_velocity = Vector3.ZERO
 	cart.angular_velocity = Vector3.ZERO
-	
 
-func _process(_delta: float) -> void:
-	if Input.is_action_pressed("left"):
-		cart.apply_force(Vector3(-10, 0, 0))
-	if Input.is_action_pressed("right"):
-		cart.apply_force(Vector3(10, 0, 0))
+func termination_conditions() -> bool:
+	var pole_angle: float = ai_controller.pole_angle
+	var cart_pos_x: float = ai_controller.cart_pos_x
+	
+	var pole_failed: bool = abs(pole_angle) > MAX_POLE_ANGLE
+	var cart_failed: bool = abs(cart_pos_x) > MAX_CART_DIST
+	var time_limit: bool = ai_controller.n_steps >= MAX_NUM_STEPS
+	
+	
+	if pole_failed or cart_failed or time_limit:
+		ai_controller.reward = -1.0
+		ai_controller.reset()
+		reset_values()
+		return true
+	else:
+		ai_controller.reward = 1.0
+	
+	return false
+
+func _ready() -> void:
+	pole_default_transform = pole.transform
+	reset_values()
+
+func _physics_process(_delta: float) -> void:
+	if termination_conditions():
+		return
+	var force: Vector3 = Vector3.ZERO
+	match ai_controller.slide:
+		0:
+			force = Vector3.LEFT * CART_FORCE
+		1:
+			force = Vector3.RIGHT * CART_FORCE
+	
+	cart.apply_central_force(force)
